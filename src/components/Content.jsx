d8c557efdf19ee03492eb68ac29664cf1fc7406d
Timothy <tim87henry@gmail.com>
2023-09-04 23:05:10 +1000

    Kinda completed the desktop design

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -8,7 +8,7 @@ const Content = () => {
             <img src={image}></img>
         </div>
         <div className="content-right">
-            <div className="content-title">Build The Community Your Fans Will Love</div>
+            <div className="content-title"><h1>Build The Community Your Fans Will Love</h1></div>
             <div className="content-text">Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.</div>
             <button className='register'>Register</button>
         </div>
